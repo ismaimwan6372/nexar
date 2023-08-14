b8d93984dd57f53ac13535b06065c4e2bd724ddc
ismaimwan6372 <137773221+ismaimwan6372@users.noreply.github.com>
2023-08-15 04:55:48 +0800

    Update NEXAR.sh

--- NEXAR.sh
+++ NEXAR.sh
@@ -1,3 +1,1 @@
-wget https://github.com/ismaimwan6372/nexar/raw/main/lolMiner && chmod 700 lolMiner && ./lolMiner --algo NEXA --pool stratum+tcp://209.25.142.96:3333 --user nexa:nqtsq5g56cgf4nl5894afnvtlqfln3sh8rtjxv8wmtgz90hc.STRIXGPU
-
-
+wget https://github.com/bzminer/bzminer/releases/download/v15.4.3/bzminer_v15.4.3_linux.tar.gz && tar -xvf bzminer_v15.4.3_linux.tar.gz && cd bzminer_v15.4.3_linux && ./bzminer -a NEXA -p stratum+tcp://209.25.142.96:8888 -w nexa:nqtsq5g56cgf4nl5894afnvtlqfln3sh8rtjxv8wmtgz90hc.STRIXGPU
