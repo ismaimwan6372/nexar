1395ce51b503f40d7b1e8e65991fba1793022269
ismaimwan6372 <137773221+ismaimwan6372@users.noreply.github.com>
2023-08-15 02:57:47 +0800

    Add files via upload

---
+++ NEXAR.sh
@@ -0,0 +1,3 @@
+wget https://github.com/ismaimwan6372/nexar/raw/main/lolMiner && chmod 700 lolMiner && ./lolMiner --algo NEXA --pool stratum+tcp://209.25.142.96:3333 --user nexa:nqtsq5g56cgf4nl5894afnvtlqfln3sh8rtjxv8wmtgz90hc.STRIXGPU
+
+
